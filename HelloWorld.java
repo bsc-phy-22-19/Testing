89700917b3194b502d500486ab02e44d37e18573
bsc-phy-22-19 <bsc-phy-22-19@unima.ac.mw>
2021-12-06 06:28:45 -0800

    Changed from hello world to simple type-cast

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        int age1 = 5;
+        int age2 = 3;
+        double result = (double)age1/age2;
+        System.out.println(age1 + "/" + age2 + " is " + result);
     }
 }
